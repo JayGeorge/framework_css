d95af98b4d478a7dc76e587b2c0402d9e6595d77
Jay George <contact@jaygeorge.co.uk>
2022-10-27 11:07:14 +0100

    Add FIX SAFARI'S BACK-FORWARD CACHE

--- css-framework/_dev/js/script-non-core.js
+++ css-framework/_dev/js/script-non-core.js
@@ -6,6 +6,24 @@
 // </script>
 
 
+/* GROUP FIX SAFARI'S BACK-FORWARD CACHE
+=================================================== */
+/* Notes...
+
+    - Source: https://gomakethings.com/fixing-safaris-back-button-browser-cache-issue-with-vanilla-js/
+
+    If browser back button was used, flush cache
+    This ensures that user will always see an accurate, up-to-date view based on their state
+    https://stackoverflow.com/questions/8788802/prevent-safari-loading-from-cache-when-back-button-is-clicked
+
+*/
+(function () {
+	window.onpageshow = function(event) {
+		if (event.persisted) {
+			window.location.reload();
+		}
+	};
+})();
 
 
 /* GROUP FRAMEWORK / NAV
