7d56c4cb2818e23b242da2b5721d30c9c6d7209d
Jay George <contact@jaygeorge.co.uk>
2023-10-09 22:21:02 +0100

    Tidy

--- css-framework/_dev/js/script-non-core.js
+++ css-framework/_dev/js/script-non-core.js
@@ -18,12 +18,13 @@
 
 */
 (function () {
-	window.onpageshow = function(event) {
-		if (event.persisted) {
-			window.location.reload();
-		}
-	};
+    window.onpageshow = function(event) {
+        if (event.persisted) {
+            window.location.reload();
+        }
+    };
 })();
+
 
 
 /* GROUP FRAMEWORK / NAV
@@ -34,7 +35,6 @@ Array.from(document.querySelectorAll('.js__navMobileButton') || []).forEach(elem
         document.querySelector('html').classList.add('js--navHasBeenOpened');
     }
 });
-
 /* GROUP FRAMEWORK / NAV / (OPTIONAL FOR "ALWAYS CLOSED" NAV) / RESET STATE ON ESCAPE
 =================================================== */
 // e.g. Give the Escape Key as an option to exit too
@@ -61,7 +61,6 @@ document.addEventListener('click', function (event) {
 
 
 
-
 /* GROUP COMPONENTS / FRAMEWORK / (NON CORE) / NAV / MULTI LEVEL
 =================================================== */
 Array.from(document.querySelectorAll('nav li a')).forEach(element => {
@@ -85,6 +84,105 @@ Array.from(document.querySelectorAll('nav li a')).forEach(element => {
         });
     };
 });
+
+
+
+/* GROUP UTILITIES / ANIMATION / IO FRAMEWORK (Authored by me)
+=================================================== */
+/* Simple Intersection Observer to for scroll-triggered animations. See my 'JavaScript.txt > IO Framework' in my wiki for more info, and animation examples in non-core.css in my CSS framework */
+/* HTML Example...
+    <div data-io>
+    </div>
+*/
+/* CSS Control using data attributes...
+    <div data-io data-io-delay="0.25">
+    </div>
+
+    [data-io-delay="0.25"] {
+        animation-delay: 0.25s;
+    }
+*/
+function jfg_intersectionObserver() {
+    const options = {
+        // When at least 25% is visible
+        threshold: [0.25]
+    }
+
+    /* GROUP UTILITIES / ANIMATION / IO FRAMEWORK / DEFAULT
+    =================================================== */
+    /* Notes...
+        [1] data-io is the default, where data-io-seen is only added once, and never removed once added
+        [2] However, data-io-currently-in-view is always added/removed as an extra hook
+    */
+    const observe_target = document.querySelectorAll('[data-io]');
+    var my_observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.setAttribute('data-io-seen', '');
+                entry.target.setAttribute('data-io-currently-in-view', '');
+            } else {
+                entry.target.removeAttribute('data-io-currently-in-view');
+            }
+        });
+    }, {
+        options
+    });
+    observe_target.forEach(entry => {
+        my_observer.observe(entry);
+    });
+    /* GROUP UTILITIES / ANIMATION / IO FRAMEWORK / MULTIPLE TIMES
+    =================================================== */
+    /* data-io-repeat can be used for multiple triggers, where it's removed once out of sight, and re-added once back on screen */
+    const observe_target_multiple_times = document.querySelectorAll('[data-io-repeat]');
+    var my_observer_multiple_times = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.intersectionRatio > 0) {
+                entry.target.setAttribute('data-io-seen', '');
+                entry.target.setAttribute('data-io-currently-in-view', '');
+            } else {
+                entry.target.removeAttribute('data-io-seen');
+                entry.target.removeAttribute('data-io-currently-in-view');
+            }
+        });
+    }, {
+        options
+    });
+    observe_target_multiple_times.forEach(entry => {
+        my_observer_multiple_times.observe(entry);
+    });
+    /* GROUP UTILITIES / ANIMATION / IO FRAMEWORK / CONTROL PLYR STATE
+    =================================================== */
+    // Leaving this here for posterity but it doesn't work because it will pause every single video on the page rather than the current one
+
+    /* Used to auto pause plyr instances when they're out of site, using intersection observer. Attach data-io-control-plyr to the same place you'd usually attach data-io-repeat to. It can be used for multiple triggers, where it's removed once out of sight, and re-added once back on screen */
+    // window.addEventListener('load', (event) => {
+    //     const observe_plyr_multiple_times = document.querySelectorAll('[data-io-control-plyr]');
+    //     var my_observer_multiple_times = new IntersectionObserver(entries => {
+    //         entries.forEach(entry => {
+    //             if (entry.intersectionRatio > 0) {
+    //                 entry.target.setAttribute('data-io-control-plyr-pause', '');
+    //                 // Resume
+    //                 Array.from(playersMutedCoverMobileAndLandscape || []).forEach(element => {
+    //                     element.play();
+    //                 });
+    //             } else {
+    //                 entry.target.removeAttribute('data-io-control-plyr-pause');
+    //                 // Pause
+    //                 Array.from(playersMutedCoverMobileAndLandscape || []).forEach(element => {
+    //                     element.pause();
+    //                 });
+    //             }
+    //         });
+    //     }, {
+    //         options
+    //     });
+    //     observe_plyr_multiple_times.forEach(entry => {
+    //         my_observer_multiple_times.observe(entry);
+    //     });
+    // });
+}
+
+jfg_intersectionObserver();
 
 
 
